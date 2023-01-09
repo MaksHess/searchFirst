ef3dc63f289b3f60c80e92b31b2597cbae960a9d
MaksHess <max.hess@imls.uzh.ch>
2023-01-09 01:04:14 +0100

    stuff for presentation

--- presentation.py
+++ presentation.py
@@ -3,14 +3,19 @@ FIND_OBJECTS_STRATEGY: {
     'multi-template': find_objects_by_multiple_template_matching,
     'threshold': find_objects_by_threshold,
     'manual': find_objects_by_manual_annotation,
-    'semiautomatic-threshold': find_objects_by_semiautomatic_annotation
+    'semiautomatic-threshold': find_objects_by_semiautomatic_annotation,
 
 }
+...
+
+
+objects, non_objects = FIND_OBJECTS_STRATEGY[args.method]
+
+
 
 # Functions need to extract their parameters from the `args` dict.
 objects, non_objects = find_objects_by_template_matching(stitched_ds, args)
 
-objects, non_objects = FIND_OBJECTS_STRATEGY[args.method]
 
 
 
